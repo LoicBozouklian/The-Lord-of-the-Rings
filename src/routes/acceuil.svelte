1b5b0e5e61dd739b4204ed66731bd10b7fd27c3c
LoicBozouklian <64065552+LoicBozouklian@users.noreply.github.com>
2020-06-03 10:09:59 +0200

    premier jet page d'accueil

--- src/routes/acceuil.svelte
+++ src/routes/acceuil.svelte
@@ -39,4 +39,19 @@
 	<figcaption>Bienvenu en Terre du Milieu</figcaption>
 </figure>
 
+<p>L’œuvre de J.R.R. Tolkien se construit au travers de dizaines de tomes écrits sur des décennies et retraçant l’histoire d’un monde fantastique entièrement créé par ses soins. Largement inspiré par les épreuves qu’il a traversées (orphelinat dans la campagne anglaise, première guerre mondiale, séparation de sa fiancée…), l’histoire de la Terre du Milieu s’étale sur Trois Âges, chacun marqué par la coexistence entre les différentes races et les conflits entre les forces du Bien et du Mal. 
+
+Les Premier et Second Âges sont traités dans le Silmarillion. 
+J.R.R. Tolkien y retrace notamment la création des elfes, des hommes et des nains par les divinités créatrices, leur installation en Terre du Milieu, la guerre entre les elfes et Morgoth, une divinité renégate, qui entraîne avec lui un apprenti, Sauron, mais aussi les guerres entre Sauron et l’alliance des Hommes et des Elfes. 
+On y retrouve également le récit de la création des royaumes des Hommes, des conflits entre les Elfes et les Nains et les relations entre la Terre du Milieu et les terres éternelles, refuges des elfes ayant fait le choix de fuir face à leur déclin à partir du Second Âge. 
+
+Le Troisième-Âge est un âge paisible qui voit les Hommes monter en puissance face au déclin des Elfes. 
+La fin de cette époque est traitée par Le Hobbit et la trilogie du Seigneur des Anneaux, c’est le temps du retour de Sauron. Le Hobbit relate la reconquête de la montagne solitaire, siège du pouvoir des Nains, jusqu’alors sous le joug du dragon du Nord, Smaug. Cette quête menée par Thorin Ecu-de-Chêne pose les fondations des évènements du Seigneur des Anneaux puisque l’on y découvre l’Anneau unique et la survie de Sauron dans la citadelle de Dol-Guldur. 
+Vous découvrirez ici l’univers du Seigneur des Anneaux et de son adaptation par Peter Jackson. 
+</p>
+
+
+
+
+
 
