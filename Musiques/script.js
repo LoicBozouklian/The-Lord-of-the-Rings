ca5eff0043d6850ded40fb138e848ae75bfbe614
RoxaneRK <64065360+RoxaneRK@users.noreply.github.com>
2020-06-12 15:18:18 +0200

    Add files via upload

---
+++ Musiques/script.js
@@ -0,0 +1,54 @@
+   function and_play_stop_one()
+   {
+   var PLAYERONE = document.getElementById("PLAYERONE" );
+   if (PLAYERONE.paused)
+   {PLAYERONE.play();}
+   else
+   {PLAYERONE.pause();}
+   }
+
+function and_play_stop_two()
+   {
+   var PLAYERTWO = document.getElementById("PLAYERTWO" );
+   if (PLAYERTWO.paused)
+   {PLAYERTWO.play();}
+   else
+   {PLAYERTWO.pause();}
+   }
+
+function and_play_stop_three()
+   {
+   var PLAYERTHREE = document.getElementById("PLAYERTHREE" );
+   if (PLAYERTHREE.paused)
+   {PLAYERTHREE.play();}
+   else
+   {PLAYERTHREE.pause();}
+   }
+
+function and_play_stop_four()
+   {
+   var PLAYERFOUR = document.getElementById("PLAYERFOUR" );
+   if (PLAYERFOUR.paused)
+   {PLAYERFOUR.play();}
+   else
+   {PLAYERFOUR.pause();}
+   }
+
+function and_play_stop_five()
+   {
+   var PLAYERFIVE = document.getElementById("PLAYERFIVE" );
+   if (PLAYERFIVE.paused)
+   {PLAYERFIVE.play();}
+   else
+   {PLAYERFIVE.pause();}
+   }
+
+function and_play_stop_six()
+   {
+   var PLAYERSIX = document.getElementById("PLAYERSIX" );
+   if (PLAYERSIX.paused)
+   {PLAYERSIX.play();}
+   else
+   {PLAYERSIX.pause();}
+   }
+
